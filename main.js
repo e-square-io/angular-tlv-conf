26f9c2ba0071db9e90aa0acea2689ffb21d7977f
Idan Sharvit <idan.sharvit@passportcard.co.il>
2024-05-20 14:53:25 +0300

    added changes

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-import './styles.scss';
+import "./styles.scss";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger-image");
+  const menu = document.querySelector(".menu");
+
+  hamburger.addEventListener("click", function () {
+    if (menu.classList.contains("show")) {
+      menu.classList.remove("show");
+    } else {
+      menu.classList.add("show");
+    }
+  });
+});
