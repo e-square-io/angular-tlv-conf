71b753ad61bdb396b220a8274240bfdde07e6039
Idan Sharvit <idan.sharvit@passportcard.co.il>
2024-05-20 11:28:48 +0300

    added responsive design for ipads

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
 import './styles.scss';
-
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburgerMenu = document.getElementById("hamburger-menu");
-
-  hamburgerMenu.addEventListener("click", function () {
-    const navLinks = document.getElementById("myLinks");
-    navLinks.classList.toggle("active");
-    this.classList.toggle("animate");
-  });
-});
